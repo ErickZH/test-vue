90fdb903010641fe1c8a2b91dc681e2cfa14db79
ErickZH <zaraterick@outlook.com>
2019-02-19 17:01:48 -0600

    Adding reminders tests.

--- test/reminders.spec.js
+++ test/reminders.spec.js
@@ -3,9 +3,43 @@ import expect from 'expect';
 import Reminders from '../src/components/Reminders.vue';
 
 describe('Remiders', () => {
-    it ('hides the reminders list if there are none', () => {
-        let wrapper = mount(Reminders);
+    let wrapper;
 
+    beforeEach(() => {
+        wrapper = mount(Reminders);
+    });
+
+    it ('hides the reminders list if there are none', () => {
         expect(wrapper.contains('ul')).toBe(false);
     });
+
+    it ('can add reminders', () => {
+        addReminder('Go to the store');
+
+        expect(wrapper.find('ul').text()).toContain('Go to the store');
+    });
+
+    it ('can remove any reminder', () => {
+        addReminder('Go to the store');
+        addReminder('Finish screencast');
+
+        let deleteButton = wrapper.find('ul > li:first-child .remove');
+
+        deleteButton.trigger('click');
+
+        expect(remindersList()).not.toContain('Go to the store');
+    });
+
+    function addReminder (body) {
+        let newReminder = wrapper.find('.new-reminder');
+
+        newReminder.element.value = body;
+        newReminder.trigger('input');
+
+        wrapper.find('button').trigger('click');
+    }
+
+    function remindersList () {
+        return wrapper.find('ul').text();
+    }
 });
